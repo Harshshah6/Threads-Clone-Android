de1e77177551d95a351f72e943948a82cebace42
harshshah6 <harshsandeep23@gmail.com>
2024-10-08 18:33:40 +0530

    update: UI

--- app/src/main/java/com/harsh/shah/threads/clone/activities/MainActivity.java
+++ app/src/main/java/com/harsh/shah/threads/clone/activities/MainActivity.java
@@ -1,21 +1,18 @@
 package com.harsh.shah.threads.clone.activities;
 
-import android.content.res.ColorStateList;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.core.widget.ImageViewCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.material.internal.EdgeToEdgeUtils;
 import com.harsh.shah.threads.clone.BaseActivity;
 import com.harsh.shah.threads.clone.R;
 import com.harsh.shah.threads.clone.databinding.ActivityMainBinding;
+import com.harsh.shah.threads.clone.fragments.ActivityNotificationFragment;
+import com.harsh.shah.threads.clone.fragments.AddThreadFragment;
+import com.harsh.shah.threads.clone.fragments.HomeFragment;
+import com.harsh.shah.threads.clone.fragments.ProfileFragment;
+import com.harsh.shah.threads.clone.fragments.SearchFragment;
 
 public class MainActivity extends BaseActivity {
 
@@ -28,7 +25,31 @@ public class MainActivity extends BaseActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        setOnClickListeners();
 
 //        ImageViewCompat.setImageTintList(binding.searchIcon, ColorStateList.valueOf(ContextCompat.getColor(this, R.color.textSec)));
+    }
+
+    private void setOnClickListeners() {
+        binding.homeIconLayout.setOnClickListener(v -> setFragment(0));
+        binding.searchIconLayout.setOnClickListener(v -> setFragment(1));
+        binding.addIconLayout.setOnClickListener(v -> setFragment(2));
+        binding.favoriteIconLayout.setOnClickListener(v -> setFragment(3));
+        binding.personIconLayout.setOnClickListener(v -> setFragment(4));
+    }
+
+    private void setFragment(int position){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        if(position == 0)
+            fragmentTransaction.replace(R.id.fragmentContainerView, HomeFragment.newInstance("","")).commit();
+        else if (position == 1)
+            fragmentTransaction.replace(R.id.fragmentContainerView, SearchFragment.newInstance("","")).commit();
+        else if(position == 2)
+            fragmentTransaction.replace(R.id.fragmentContainerView, AddThreadFragment.newInstance("","")).commit();
+        else if (position == 3)
+            fragmentTransaction.replace(R.id.fragmentContainerView, ActivityNotificationFragment.newInstance("","")).commit();
+        else if (position == 4)
+            fragmentTransaction.replace(R.id.fragmentContainerView, ProfileFragment.newInstance("","")).commit();
     }
 }
--- app/src/main/java/com/harsh/shah/threads/clone/fragments/HomeFragment.java
+++ app/src/main/java/com/harsh/shah/threads/clone/fragments/HomeFragment.java
@@ -2,7 +2,12 @@ package com.harsh.shah.threads.clone.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,6 +30,8 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    RecyclerView recyclerView;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -55,6 +62,8 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
@@ -62,5 +71,46 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new Adapter());
+    }
+
+    class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = viewType == 1 ? LayoutInflater.from(getContext()).inflate(R.layout.main_head_layout, null) : LayoutInflater.from(getContext()).inflate(R.layout.home_list_item, null);
+            view.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 20;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return position==0?1:0;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+        }
     }
 }
